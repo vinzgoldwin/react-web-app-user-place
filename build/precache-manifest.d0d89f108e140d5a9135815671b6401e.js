995c5cc0d940ec0e43ee8453b91b152b5c2c4384
Kevin Goldwin <vinzgoldwin@gmail.com>
2021-08-23 20:45:14 +0700

    Ready to deploy

---
+++ build/precache-manifest.d0d89f108e140d5a9135815671b6401e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a147ec425005e4c101bd602ba6ab648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bf556210419c1ec2bcc",
+    "url": "/static/css/0.0b4b2ab9.chunk.css"
+  },
+  {
+    "revision": "f937f55888a8f3c3bdcc",
+    "url": "/static/css/4.028ac262.chunk.css"
+  },
+  {
+    "revision": "2806c725fac38f7c842f",
+    "url": "/static/css/5.f818987d.chunk.css"
+  },
+  {
+    "revision": "cfd1ac7d4b63bcb39480",
+    "url": "/static/css/6.59ccae76.chunk.css"
+  },
+  {
+    "revision": "9aa0c9c5346db216822a",
+    "url": "/static/css/7.210280e6.chunk.css"
+  },
+  {
+    "revision": "40718fa6936cf2d36801",
+    "url": "/static/css/8.2cfb3aef.chunk.css"
+  },
+  {
+    "revision": "f1fc1edbe5d85f93674e",
+    "url": "/static/css/main.b89feb85.chunk.css"
+  },
+  {
+    "revision": "7bf556210419c1ec2bcc",
+    "url": "/static/js/0.6a93510d.chunk.js"
+  },
+  {
+    "revision": "1e2e3f7030a961937eb6",
+    "url": "/static/js/3.68821f84.chunk.js"
+  },
+  {
+    "revision": "f937f55888a8f3c3bdcc",
+    "url": "/static/js/4.6073a90b.chunk.js"
+  },
+  {
+    "revision": "2806c725fac38f7c842f",
+    "url": "/static/js/5.431d9aaa.chunk.js"
+  },
+  {
+    "revision": "cfd1ac7d4b63bcb39480",
+    "url": "/static/js/6.98cb9c6f.chunk.js"
+  },
+  {
+    "revision": "9aa0c9c5346db216822a",
+    "url": "/static/js/7.06eea577.chunk.js"
+  },
+  {
+    "revision": "40718fa6936cf2d36801",
+    "url": "/static/js/8.36e0e60b.chunk.js"
+  },
+  {
+    "revision": "f1fc1edbe5d85f93674e",
+    "url": "/static/js/main.673b7de4.chunk.js"
+  },
+  {
+    "revision": "b10ebf767d71ee9f529a",
+    "url": "/static/js/runtime-main.107dd36a.js"
+  }
+]);
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+
+const UserPlaces = () => {
+  const [loadedSpaces, setLoadedSpaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const userId = useParams().userId;
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`
+        );
+        setLoadedSpaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  const placeDeletedHandler = (deletedPlaceId) => {
+    setLoadedSpaces((prevPlaces) =>
+      prevPlaces.filter((place) => place.id !== deletedPlaceId)
+    );
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedSpaces && (
+        <PlaceList items={loadedSpaces} onDeletePlace={placeDeletedHandler} />
+      )}
+    </React.Fragment>
+  );
+};
+
+export default UserPlaces;
